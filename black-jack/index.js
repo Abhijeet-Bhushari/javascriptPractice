57b8c3299451dcf9b2b5ef90d923ff959019268e
Abhijeet-Bhushari <115354940+Abhijeet-Bhushari@users.noreply.github.com>
2023-10-26 21:02:01 +0530

    black jack game completed

--- black-jack/index.js
+++ black-jack/index.js
@@ -1,26 +1,124 @@
-let firstCard = 4;
-let secondCard = 5;
-function addCard(player, card){
-    player += card;
+let isBJ = false;
+let isAlive = true;
+let cardSum = 0;
+let message = "";
+let cardsArr = [];
+let messageEl = document.querySelector("#message");
+let cards = document.querySelector("#cards");
+let sum = document.querySelector("#sum");
+let startEl = document.querySelector("#start");
+let newCardEl = document.querySelector("#new-card");
+let restartEl = document.querySelector("#restart");
+let buyChipsEl = document.querySelector("#buy-chips");
+
+let player = {
+  name: "Abhi",
+  chips: 10,
 }
 
-let sum = firstCard + secondCard;
-console.log(sum);
+let playerEl = document.querySelector("#player");
 
-let player1 = 0, player2 = 0;
+playerEl.textContent = player.name + ": $" + player.chips;
+console.log(player.name);
+console.log(player.chips);
 
-addCard(player1, 3);
-addCard(player2, 10);
-addCard(player1, 10);
-addCard(player2, 12);
+function buyChips(){
+  player.chips = 100;
+  restart();
+}
 
-console.log(player1 + " " + player2);
+function setChips(x){
+  player.chips = x;
+  playerEl.textContent = player.name + ": $" + x;
+}
 
-if(player1 > 21) console.log("Player1 lost");
-else if(player2 > 21) console.log("Player2 lost");
-else if(player1 < player2) console.log("Player2 won");
-else if(player1 > player2) console.log("Player1 won");
-else console.log("oh! That's a tie"); 
+restart();
 
+function outOfChips(){
+  messageEl.textContent = player.name + ", You are out of chips"
+  cards.style.display = "none";
+  sum.style.display = "none";
+  startEl.style.display = "none";
+  restartEl.style.display = "none";
+  buyChipsEl.style.display = "block";
+}
 
+function restart(){
+  if(player.chips == 0) outOfChips();
+  else{
+  setChips(player.chips);
+  cards.style.display = "block";
+  sum.style.display = "block";
+  startEl.style.display = "block";
+  buyChipsEl.style.display = "none";
+  cards.textContent = "Cards: ";
+  cardSum = 0;
+  message = "";
+  cardsArr = [];
+  messageEl.textContent = "Want to play a new round?";
+  sum.textContent = "Sum: ";
+  isBJ = false;
+  isAlive = true;
+  restartEl.style.display = "none";
+  startEl.style.display = "block";
+  }
+}
 
+function setRandom(){
+    let card = Math.floor(Math.random() * 100) % 13;  
+    if (card === 0) card = 11;
+    else if (card >= 10) card = 10;
+    else card += 1;
+    return card;
+}
+
+function displayCards(){
+    let cardsText = "Cards: ";
+    for(let i=0; i<cardsArr.length; i++){
+        cardsText += " " + cardsArr[i];
+    }
+    cards.textContent = cardsText;
+}
+
+function startGame() {
+  cardsArr.push(setRandom());
+  cardsArr.push(setRandom());
+  displayCards();
+  cardSum += cardsArr[0] + cardsArr[1];
+  sum.textContent = "Sum: " + cardSum;
+  checkBlackjack();
+}
+
+function checkBlackjack() {
+  if (cardSum === 21) {
+    message = "you have blackjack";
+    isBJ = true;
+    setChips(player.chips*10);
+    newCardEl.style.display = "none";
+    startEl.style.display = "none";
+    restartEl.style.display = "block";
+  } 
+  else if (cardSum > 21) {
+    message = "you are out of the game";
+    isAlive = false;
+    setChips(player.chips - 10);
+    newCardEl.style.display = "none";
+    startEl.style.display = "none";
+    restartEl.style.display = "block";
+  } 
+  else {
+    message = "Do you want to draw a new card?";
+    startEl.style.display = "none";
+    newCardEl.style.display = "block";
+  }
+  messageEl.textContent = message;
+  console.log(message);
+}
+
+function newCard(){
+    cardsArr.push(setRandom());
+    displayCards();
+    cardSum += cardsArr[cardsArr.length - 1];
+    sum.textContent = "Sum: " + cardSum;
+    checkBlackjack();
+}
